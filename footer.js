e7cebd3122ec1a5a4b70b3acf85726fd612c9251
tanvGUPTA1234 <92089191+tanvGUPTA1234@users.noreply.github.com>
2022-02-10 14:26:59 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,172 @@
+< footer >
+    <
+    div class = "footer-content" >
+    <
+    img src = "http://lbimg.in.com/LiveBlog/img/2021/04/36059d29de444bae87c4ca7d675de42b.png"
+class = "logo"
+alt = "" >
+    <
+    div class = "footer-ul-container" >
+    <
+    ul class = "category" >
+    <
+    li class = "category-title" > Smartphone < /li> <
+li >
+    <
+    a href = "#"
+class = "footer-link" > vivo < /a>  <
+    a href = "vivo" > vivo < /a> </li >
+
+    <
+    li > < a href = "#"
+class = "footer-link" > OPPO < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > Realeme < /a></li >
+    <
+    li >
+    <
+    a href = "#"
+class = "footer-link" > sumsung < /a></li >
+    <
+    li > < a href = "# "
+class = "footer-link" > TEchno < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > Radmie < /a> </a >
+    <
+    /li>  <
+    li > < a href = "#"
+class = "footer-link" > lava < /a></li >
+
+    <
+    /ul>  <
+    ul class = "category" >
+    <
+    li class = "category-title" > iphone < /li>  <
+    li > < a href = "#"
+class = "footer-link" > Apple < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > iPod 1 st Gen < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > keypad < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > iPhone 3 G < /a></li >
+    <
+    li > < a href = "#"
+lass = "footer-link" > iPod 2 nd Gen < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > iPad 1 st Gen < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > casuals < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > formals < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > sports < /a></li >
+    <
+    li > < a href = "#"
+class = "footer-link" > watch < /a></li >
+    <
+    /ul> </div > < /div> </footer >
+
+    <
+    footer >
+
+    <
+    p class = "footer-title" > about company < /p>  <
+    p class = "info" > Lorem ipsum, dolor sit amet consectetur adipisicing elit.Repellat tempore ad suscipit, eos eius quisquam sed optio nisi quaerat fugiat ratione et vero maxime praesentium, architecto minima reiciendis iste quo deserunt assumenda alias ducimus.
+Ullam odit maxime id voluptates rerum tenetur corporis laboriosam!Cum error ipsum laborum tempore in rerum necessitatibus nostrum nobis modi!Debitis adipisci illum nemo aperiam sed, et accusamus ut officiis.Laborum illo exercitationem quo
+culpa reprehenderit excepturi distinctio tempore cupiditate praesentium nisi ut iusto, assumenda perferendis facilis voluptas autem fuga sunt ab debitis voluptatum harum eum.Asperiores, natus!Est deserunt incidunt quasi placeat omnis, itaque
+harum ?
+    <
+    /p>  <
+    p class = "info" > Mr MUKESH Khandelwal < /p>  <
+    p class = "info" > phone no : 94616405152 < /p>  <
+    p class = "info" > phone no: 70142046630 < /p>
+
+<
+div class = "footer-social-container" >
+    <
+    div >
+    <
+    a href = "#"
+class = "social-link" > terms & services < /a> <
+a href = "#"
+class = "social-link" > privacy page < /a> < /
+    div > <
+    div >
+    <
+    a href = "#"
+class = "social-link" > instagram < /a> <
+a href = "#"
+class = "social-link" > facebook < /a> <
+a href = "#"
+class = "social-link" > twitter < /a> < /
+    div > <
+    /div> <
+p class = "footer-credit" > Clothing, Best apparels online store < /p> < /
+    footer >
+
+
+
+
+    const createFooter = () => {
+        let footer = document.querySelector('footer');
+
+        footer.innerHTML = `
+    <div class="footer-content">
+        <img src="https://www.reliancedigital.in/medias/VIVO-Y21-Smartphones-491998268-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w1OTYxM3xpbWFnZS9qcGVnfGltYWdlcy9oMzYvaGZkLzk2NTk3MDI5NjgzNTAuanBnfGZiYTdjNDFhMmY5NjExZGNmYmJhM2VjNGFkZWQ2MGY5Y2RiNjdmN2UzMmFmYjhjOWE2YTgxZDhjMjFmYjUyNjA" alt="">
+        <div class="footer-ul-container">
+            <ul class="category">
+                <li class="category-title">Sartphone</li>
+                <li><a href="#" class="footer-link">vivo</a></li>
+                <li><a href="#" class="footer-link">OPPO</a></li>
+                <li><a href="#" class="footer-link">Releme</a></li>
+                <li><a href="#" class="footer-link">sumsung</a></li>
+                <li><a href="#" class="footer-link">Anvcg</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watch</a></li>
+            </ul>
+            <ul class="category">
+                <li class="category-title">iphone</li>
+                <li><a href="#" class="footer-link">Apple</a></li>
+                <li><a href="#" class="footer-link"></a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watch</a></li>
+            </ul>
+        </div>
+    </div>
+    <p class="footer-title">about company</p>
+    <p class="info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat tempore ad suscipit, eos eius quisquam sed optio nisi quaerat fugiat ratione et vero maxime praesentium, architecto minima reiciendis iste quo deserunt assumenda alias ducimus. Ullam odit maxime id voluptates rerum tenetur corporis laboriosam! Cum error ipsum laborum tempore in rerum necessitatibus nostrum nobis modi! Debitis adipisci illum nemo aperiam sed, et accusamus ut officiis. Laborum illo exercitationem quo culpa reprehenderit excepturi distinctio tempore cupiditate praesentium nisi ut iusto, assumenda perferendis facilis voluptas autem fuga sunt ab debitis voluptatum harum eum. Asperiores, natus! Est deserunt incidunt quasi placeat omnis, itaque harum?</p>
+    <p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
+    <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">terms & services</a>
+            <a href="#" class="social-link">privacy page</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+        </div>
+    </div>
+    <p class="footer-credit">Clothing, Best apparels online store</p>
+    `;
+    }
---
+++ nav.js
@@ -0,0 +1,63 @@
+  const createNav = () => {
+          let nav = document.querySelector('.navbar');
+
+          nav.innerHTML = '
+
+          <
+          div class = "nav " >
+              <
+              img src = "https://fdn2.gsmarena.com/vv/pics/vivo/vivo-v19-dual-selfie-3.jpg "
+          class = "brand-logo "
+          alt = " " >
+              <
+              div class = "nav-items " >
+              <
+              div class = "search " >
+              <
+              input type = "text "
+          class = "search-box "
+          placeholder = "search  " >
+              <
+              button class = "search-btn " > search < /button> < /
+              div > <
+              a href = "# " > < img src = "https://fdn2.gsmarena.com/vv/pics/vivo/vivo-v20-1.jpg "
+          widht = "50 "
+          height = "100 "
+          alt = "vivo "
+          align = "bottom/left " / > < /a>
+
+          <
+          a href = "# " >
+              <
+              img src = "https://fdn2.gsmarena.com/vv/pics/vivo/vivo-v20-1.jpg "
+          width = "700 "
+          height = "500 "
+          align = "down-right " >
+              <
+              /a>
+
+
+          <
+          /div> < /
+          div > <
+              /nav>
+
+          <
+          ul class = "links-container " >
+              <
+              li class = "link-item " > < a href = "# "
+          class = "link " > home < /a></li >
+              <
+              li class = "link-item " > < a href = "# "
+          class = "link " > vivo < /a></li >
+              <
+              li class = "link-item " > < a href = "# "
+          class = "link " > OPPO < /a></li >
+              <
+              li class = "link-item " > < a href = "# "
+          class = "link " > Realme < /a></li >
+              <
+              li class = "link-item " > < a href = "# "
+          class = "link " > iphone < /a></li >
+              <
+              /ul>
